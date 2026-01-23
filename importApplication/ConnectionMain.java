4908388ed10349d9a3945dca22d1fabd8b1399b5
rhit-ciminodj <ciminodj@rose-hulman.edu>
2026-01-22 20:33:13 -0500

    Changes to connecton, and fixed json file.

---
+++ importApplication/ConnectionMain.java
@@ -0,0 +1,49 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionMain {
+
+	private final String url = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}};encrypt=false;";
+
+	private Connection connection = null;
+
+	private final String databaseName;
+	private final String serverName;
+
+	public ConnectionMain(String serverName, String databaseName) {
+		this.serverName = serverName;
+		this.databaseName = databaseName;
+	}
+
+	public boolean connect(String user, String pass) {
+		String fullUrl = url
+            .replace("${dbServer}", this.serverName)
+            .replace("${dbName}", this.databaseName)
+            .replace("${user}", user)
+            .replace("${pass}", pass);
+
+		try {
+			this.connection = DriverManager.getConnection(fullUrl);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+
+	public Connection getConnection() {
+		return this.connection;
+	}
+	
+	public void closeConnection() {
+		try {
+            if (this.connection != null && !this.connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Error closing connection: " + e.getMessage());
+        }
+	}
+
+};
