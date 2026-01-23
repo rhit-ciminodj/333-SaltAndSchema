658616654115805d0eef7d2c231788decfc03e5c
rhit-hungra <hungra@rose-hulman.edu>
2026-01-23 12:33:58 -0500

    More sprocs

--- importApplication/AddtoDatabase.java
+++ importApplication/AddtoDatabase.java
@@ -28,11 +28,57 @@ public class AddtoDatabase {
 			stmt.execute();
 			return true;
 		} catch (SQLException e) {
-			if(e.getErrorCode() == 54001) {
+			if(e.getErrorCode() == 51021) {
 				e.printStackTrace();
 			}
-			if(e.getErrorCode() == 54002) {
+			if(e.getErrorCode() == 51022) {
 				JOptionPane.showMessageDialog(null, "Restaurant name already exists.");
+			}
+			e.printStackTrace();
+			return false;
+		}
+    }
+
+    public Boolean addEquipToRecipe(int RecipeID, int EquipmentID) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call addEquipToRecipe(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, RecipeID);
+			stmt.setInt(3, EquipmentID);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+    }
+
+
+    public Boolean addIngredientToRecipe(int IngredientID, int RecipeID, int Quantity) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call addIngredientToRecipe(?,?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, IngredientID);
+			stmt.setInt(3, RecipeID);
+            stmt.setInt(4, Quantity);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
 			}
 			e.printStackTrace();
 			return false;
