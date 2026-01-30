a7c94e5fda2e99e28b08c7029246c61d9b1bce04
rhit-gryniehd <gryniehd@rose-hulman.edu>
2026-01-29 23:32:45 -0500

    Add

--- importApplication/AddtoDatabase.java
+++ importApplication/AddtoDatabase.java
@@ -84,5 +84,211 @@ public class AddtoDatabase {
 			return false;
 		}
     }
+	public Boolean addSubstituteToIngredient(int IngredientID, int SubstituteID) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call addSubstituteToIngredient(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, IngredientID);
+			stmt.setInt(3, SubstituteID);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean changeAddress(int RestaurantID, String newAddress) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call changeAddress(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, RestaurantID);
+			stmt.setString(3, newAddress);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean changePriceOfItem(int MenuItemID, double newPrice) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call changePriceOfItem(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, MenuItemID);
+			stmt.setDouble(3, newPrice);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean changeUsername(int UserID, String newUsername) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call changeUsername(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, UserID);
+			stmt.setString(3, newUsername);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean DeleteUser(int UserID) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call DeleteUser(?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, UserID);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean getRecipeStars(int RecipeID, double newStars) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call getRecipeStars(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, RecipeID);
+			stmt.setDouble(3, newStars);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean linkRecipeToRestaurant(int RecipeID, int RestaurantID) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call linkRecipeToRestaurant(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setInt(2, RecipeID);
+			stmt.setInt(3, RestaurantID);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				e.printStackTrace();
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean newCuisine(String CuisineName) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call newCuisine(?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setString(2, CuisineName);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				JOptionPane.showMessageDialog(null, "Cuisine name already exists.");
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean newEquipment(String EquipmentName) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call newEquipment(?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setString(2, EquipmentName);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				JOptionPane.showMessageDialog(null, "Equipment name already exists.");
+			}
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public Boolean newGroceryStore(String StoreName, String Location) {	
+		try {
+			CallableStatement stmt;
+			stmt = dbService.getConnection().prepareCall("{? = call newGroceryStore(?,?)}");
+			stmt.registerOutParameter(1, java.sql.Types.INTEGER);
+			stmt.setString(2, StoreName);
+			stmt.setString(3, Location);
+
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 51021) {
+				e.printStackTrace();
+			}
+			if(e.getErrorCode() == 51022) {
+				JOptionPane.showMessageDialog(null, "Grocery Store name already exists.");
+			}
+			e.printStackTrace();
+			return false;
+		}
 
 }
